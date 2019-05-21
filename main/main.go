538a183a9c5513049849b2d3fe7c9323eaa536ef
sunchuanzhi <sunchuanzhi@bytedance.com>
2019-05-21 09:51:11 +0800

    hello

--- main/main.go
+++ main/main.go
@@ -1,9 +1,17 @@
 package main
 
-import "mypack"
+import (
+        "mypack"
+        "fmt"
+)
 //main函数
 func main(){
 	//mypack.Run_v1()
 	mypack.Run_v2()
 	//mypack.Run_v3()
 }
+
+func show(){
+   fmt.Printf("%s",hello)
+   }
+
